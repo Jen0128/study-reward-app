4dfd9383298a1890990351e46e1998da38646caf
Your Name <jennn.0128@gmail.com>
2026-07-27 13:10:04 +0800

    Extract CSS styles into style.css

---
+++ config.js
@@ -0,0 +1,12 @@
+// 專案全域設定檔
+const CONFIG = {
+  // Google Apps Script Web App API 網址
+  API_URL: "https://script.google.com/macros/s/AKfycbyZ8ls-B00xg4CiSlTJo9-dRCg3kn21xEtSeKk1isLSQD3lko9PtTabPLrQhIfuuTguow/exec",
+  
+  // 身分驗證 PIN 碼 (預設: 男友 1111 / 女友 8888)
+  PIN_BF: "1111",
+  PIN_GF: "8888",
+
+  // Google Drive 照片資料夾 ID
+  FOLDER_ID: "1yFzNgQi4boYLtmhOr-WZiZ4AxWK5FT-z"
+};

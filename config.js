a9189e318d17987b1d26048a73d45d747b2c781b
Your Name <jennn.0128@gmail.com>
2026-07-27 16:45:33 +0800

    倉庫同步

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 專案全域設定檔
 const CONFIG = {
 
-  API_URL: "https://script.google.com/macros/s/AKfycbwuZ4HBj1pC6O3QYtQldUAPlLbZDDZIi2wBiNLaHS_G-YR_Oh69gcWFwsPE4QLfaX3DgA/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbza4FhAMMV-MEWNErSRJVJbvCBW8svuSv7WT7_rJPJ3JjMBlDQoocn2ZNdOwPhL-RRN/exec",
   
   PIN_BF: "0128",
   PIN_GF: "8888",
